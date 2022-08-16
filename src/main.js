40c1b3b5a8aafa28514b13d182eaf57de01dcda9
NattaBravo <fiido.project@gmail.com>
2022-08-16 13:33:52 -0500

    Prueba 1. Pintar los datos en html incompleto en espera de sincronizar con la madre

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
-//import { example } from './data.js';
-// import data from './data/lol/lol.js';
+
 import pokemon from './data/pokemon/pokemon.js';
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(data);
 
@@ -13,55 +11,34 @@ console.log(data);
 
 let infoPokemon = Object.values(data.pokemon);
 
-function showData(datos) {
+function showData(data) {
     for (let i = 0; i < pokemon.length; i++){
         let namePokemon = pokemon[i].name;
-        let imagenPokemon = pokemon[i].img;
-        let contenedor = document.querySelector(".contenedor");
+        let imagePokemon = pokemon[i].img;
+        console.log(imagePokemon);
+        let showBox = document.querySelector("showBox");
         let info = document.createElement("div");
-        contenedor.appendChild(info)
-        info.innerHTML= `
-                <img src= "${imagenpokemon}"></img>
-        <p>${namePokemon}</p>
-        `
-        
-  //      
+        showBox.appendChild(info)
+        info.innerHTML= 
+        `<img src= "${imagePokemon}"></img>
+        <p>${namePokemon}</p>`
     }
 }
-window.addEventListener("load",showData(pokemon));
+window.addEventListener("load", showData(pokemon))
 
 
-const menuPokebook =  {
+///const menuPokebook =  {
 ///    home: mainPage,
 ///    Pokedex: secondPage,
 ///    Types: thirdPage,
-}
+///}
 
-function mainPage (){
-    document.getElementById("homePage").style.display = "none";
-    document.getElementById("page2Pokedex").style.display="block";
-
-}
-///const secondPage = function (){
+///function mainPage (){
 ///    document.getElementById("homePage").style.display = "none";
 ///    document.getElementById("page2Pokedex").style.display="block";
 ///}
-///const mainPage = function (){
-///    document.getElementById("homePage").style.display = "none";
-///    document.getElementById("page2Pokedex").style.display="block";
-///};
 
-document.getElementById("mainMenu").addEventListener("change", function(){
-    let stringmenu = document.menuForm.menu[document.menuForm.menu.selectedIndex].value;
-    stringmenu = parseInt(stringmenu);
-    console.log(stringmenu);
-    
-///    for (i = 0; i<menuPokebook.length; i++){
-///        let positionInObject = 
-///}
-
-
-///    if () {
-///    document.getElementById("homePage").style.display = "none";
-///    document.getElementById("page2Pokedex").style.display="block";
-})
+///document.getElementById("mainMenu").addEventListener("change", function(){
+///    let stringmenu = document.menuForm.menu[document.menuForm.menu.selectedIndex].value;
+///    stringmenu = parseInt(stringmenu);
+///    console.log(stringmenu);
