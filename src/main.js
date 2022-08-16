b456812e944938e2a9cc719f277197afb37f5976
NattaBravo <fiido.project@gmail.com>
2022-08-16 17:38:01 -0500

    Actualizacion para conectividad con carpeta CSS y organizacion de HTML

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
+
 import data from './data/pokemon/pokemon.js'; //trajimos la data en console log
+
 
 let beginning= document.getElementById ("beginning");
 beginning.addEventListener ("click",changeViewBeginning);
@@ -27,31 +29,18 @@ console.log(data);
 
 let infoPokemon = Object.values(data.pokemon);
 
-function showData(data) {
-    for (let i = 0; i < pokemon.length; i++){
-        let namePokemon = pokemon[i].name;
-        let imagePokemon = pokemon[i].img;
-        console.log(imagePokemon);
-        let showBox = document.querySelector("showBox");
+function showData(infoPokemon) {
+    for (let i = 0; i < infoPokemon.length; i++){
+        let namePokemon = infoPokemon[i].name;
+        let imagePokemon = infoPokemon[i].img;
+        console.log(namePokemon);
+        let showBox = document.querySelector("selectorBox");
         let info = document.createElement("div");
-        contenedor.appendChild(info)
-        info.innerHTML= `
+        selectorBox.appendChild(info)
+        infoPokemon.innerHTML= `
                 <img src= "${imagenpokemon}"></img>
         <p>${namePokemon}</p>
         `
     }
 }
-window.addEventListener("load", showData(pokemon))
-
-
-function mainPage (){
-    document.getElementById("homePage").style.display = "none";
-    document.getElementById("page2Pokedex").style.display="block";
-
-}
-
-document.getElementById("mainMenu").addEventListener("change", function(){
-    let stringmenu = document.menuForm.menu[document.menuForm.menu.selectedIndex].value;
-    stringmenu = parseInt(stringmenu);
-    console.log(stringmenu);
-})
+window.addEventListener("load", showData(data))
