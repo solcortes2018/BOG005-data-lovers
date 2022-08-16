fb343edeea90d3521f97eac44efee16d2fe22f2c
solcortes2018 <solecito-2109@hotmail.com>
2022-08-16 12:31:59 -0500

    cambio entre paginas del menu principal

--- src/main.js
+++ src/main.js
@@ -4,3 +4,25 @@ let pokemon = Object.values(data.pokemon);
 
 console.log(data);
 
+let beginning= document.getElementById ("beginning");
+beginning.addEventListener ("click",changeViewBeginning);
+let pokedexButton= document.getElementById("pokedexButton");
+pokedexButton.addEventListener("click",changeViewPokedexButton);
+let typesButton= document.getElementById("typesButton");
+typesButton.addEventListener("click",changeViewTypesButton);
+
+function changeViewBeginning() {
+    document.getElementById("containerTwo","containerThree").style.display = "none";
+    document.getElementById("containerOne").style.display = "block";
+}
+
+
+function changeViewPokedexButton() {
+    document.getElementById("containerOne","containerTwo").style.display = "none";
+    document.getElementById("containerThree").style.display = "block";
+}
+
+function changeViewTypesButton() {
+    document.getElementById("containerOne","containerThree").style.display = "none";
+    document.getElementById("containerTwo").style.display = "block";
+}
