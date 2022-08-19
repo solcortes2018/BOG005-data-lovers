6109bef524c00078bc2c49c2d1b4ca73c5e1a45f
solcortes2018 <solecito-2109@hotmail.com>
2022-08-19 09:40:25 -0500

    cambios prueba

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 import data from './data/pokemon/pokemon.js'; //trajimos la data en console log prueba #1
 
 
-console.log(pokemon);
+//console.log(pokemon);//
 
 let beginning= document.getElementById ("beginning");
 beginning.addEventListener ("click",changeViewBeginning);
@@ -27,12 +27,35 @@ function changeViewTypesButton() {
     document.getElementById("containerThree").style.display = "block";
 }
 
+const dataPoke = POKEMON.pokemon;
+// Mostrando con template
+const template = (list) => {
+  let templateListPoke = '';
+  // recorremos nuestro array con forEach
+  list.forEach((dataPoke) => {
+    // creamos un template(string) por cada elemento del array
+    const card = `
+      <div class='card-link'>
+        <article class='blog-card'>
+          <img class='post-image' src='${dataPoke.img}' />
+          <h3 class='post-id'>${dataPoke.id}</h3>
+          <div class='article-details'>
+            <h4 class='post-title'>${dataPoke.name}</h4>
+            <h6 class='post-category'>${dataPoke.type}</h6>
+          </div>
+        </article>
+      </div>
+    `;
+    templateListPoke += card;
+  });
+  document.getElementById('targetPokemon').innerHTML = templateListPoke;
+};
+template(dataPoke);
+//console.log(data);
 
-console.log(data);
+//let infoPokemon = Object.values(data.pokemon);
 
-let infoPokemon = Object.values(data.pokemon);
-
-function showData(infoPokemon) {
+/*function showData(infoPokemon) {
     for (let i = 0; i < infoPokemon.length; i++ ){
         let namePokemon = infoPokemon[i].name;
         let createNameForPokemon=document.createTextNode(namePokemon);
@@ -45,7 +68,7 @@ function showData(infoPokemon) {
         let createInfo = document.createElement("div");
 
         showBox.appendChild(createInfo);
-        createInfo.appendChild(createNameForPokemon);
+        createInfo.appendChild(createNameForPokemon);//
        /// createInfo.appendChild(createImageForPokemon);
        
         
@@ -55,5 +78,4 @@ function showData(infoPokemon) {
         ///<p>${namePokemon}</p>
        /// `
     }
-}
-window.addEventListener("load", showData(infoPokemon));
+}*/
