78f326c9d220ee70d9574e364a15e9c8e7682e60
solcortes2018 <solecito-2109@hotmail.com>
2022-08-20 07:38:44 -0500

    numero de pokemon, tipo , hyml responsive

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@
 import data from './data/pokemon/pokemon.js'; //trajimos la data en console log prueba #1
 
 
-//console.log(pokemon);//
-
 let beginning= document.getElementById ("beginning");
 beginning.addEventListener ("click",changeViewBeginning);
 let pokedexButton= document.getElementById("pokedexButton");
@@ -27,53 +25,42 @@ function changeViewTypesButton() {
     document.getElementById("containerThree").style.display = "block";
 }
 
-const dataPoke = POKEMON.pokemon;
-// Mostrando con template
-const template = (list) => {
-  let templateListPoke = '';
-  // recorremos nuestro array con forEach
-  list.forEach((dataPoke) => {
-    // creamos un template(string) por cada elemento del array
-    const card = `
-      <div class='card-link'>
-        <article class='blog-card'>
-          <img class='post-image' src='${dataPoke.img}' />
-          <h3 class='post-id'>${dataPoke.id}</h3>
-          <div class='article-details'>
-            <h4 class='post-title'>${dataPoke.name}</h4>
-            <h6 class='post-category'>${dataPoke.type}</h6>
-          </div>
-        </article>
-      </div>
-    `;
-    templateListPoke += card;
-  });
-  document.getElementById('targetPokemon').innerHTML = templateListPoke;
-};
-template(dataPoke);
-//console.log(data);
 
-//let infoPokemon = Object.values(data.pokemon);
+console.log(data);
 
-/*function showData(infoPokemon) {
+let infoPokemon = Object.values(data.pokemon);
+
+function showData(infoPokemon) {
     for (let i = 0; i < infoPokemon.length; i++ ){
         let namePokemon = infoPokemon[i].name;
         let createNameForPokemon=document.createTextNode(namePokemon);
         ///console.log(namePokemon);
         let imagePokemon = infoPokemon[i].img;
         console.log(imagePokemon);
-        ///let createImageForPokemon=document.createElement(imagePokemon);
+        ///let createImageForPokemon= (document.createElement(`<img src= "${imagePokemon}"></img>`));
         let showBox = document.querySelector(".selectorBox");
         let createInfo = document.createElement("div");
+      //numero de pokemon
+        let numberPokemon=infoPokemon[i].num;
+        let createNumberForPokemon=document.createTextNode(numberPokemon);
+        console.log(numberPokemon);
+        //tipo de pokemon
+        let typesPokemon=infoPokemon [i].type;
+        let createTypesForPokemon=document.createTextNode(typesPokemon);
+        console.log(typesPokemon);
+
         showBox.appendChild(createInfo);
-        createInfo.appendChild(createNameForPokemon);//
-       /// createInfo.appendChild(createImageForPokemon);
-       
-        
-        
-       // infoPokemon.innerHTML= `
-       //         <img src= "${imagePokemon}"></img>
-        ///<p>${namePokemon}</p>
-       /// `
+        createInfo.innerHTML=`
+        <div class="stylePokemon">
+        <img  src= "${imagePokemon}">
+        <p id=numPokemon>${numberPokemon}</p>
+        <p id=typesPokemon>${typesPokemon}</p>
+        </div>`
+        createInfo.appendChild(createNameForPokemon);
     }
-}*/
+}
+window.addEventListener("load", showData(infoPokemon));
+
+document.getSelection("div").addEventListener("click", function(){ 
+}
+)
